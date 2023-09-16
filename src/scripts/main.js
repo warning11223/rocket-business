6181cea2103805b91e494de834b7c2714049560c
warning11223 <warning11223@gmail.com>
2023-09-16 22:35:05 +0300

    refactor: carousel; footer; header; info;

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,64 +1,55 @@
-// Находим элементы на странице
 const openPopupButton = document.querySelector(".header__right__btn");
 const carouselBtn = document.querySelectorAll(".carousel__desc__btn--green");
 const closePopupButton = document.querySelector("#close-popup");
 const popup = document.querySelector("#popup");
 const navBtn = document.querySelector(".nav__btn");
+const submitBtn = document.querySelector(".submit-button");
 
-// Функция для открытия всплывающего окна
 function openPopup() {
   popup.style.display = "flex";
 }
 
-// Функция для закрытия всплывающего окна
 function closePopup() {
   popup.style.display = "none";
 }
 
-// Функция для закрытия всплывающего окна при клике вне окна
 function closePopupOutsideClick(event) {
   if (event.target === popup) {
     closePopup();
   }
 }
 
-// Добавляем обработчик события для клика на документе
 document.addEventListener("click", closePopupOutsideClick);
-
-// Добавляем обработчики событий
+submitBtn.addEventListener("click", closePopup);
 openPopupButton.addEventListener("click", openPopup);
 navBtn.addEventListener("click", openPopup);
 carouselBtn.forEach((item) => item.addEventListener("click", openPopup));
 closePopupButton.addEventListener("click", closePopup);
 
 // Функция для отправки данных формы на сервер
-function submitForm(event) {
-  event.preventDefault(); // Предотвращаем стандартное действие отправки формы
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // Предотвращаем стандартное поведение отправки формы
 
-  const form = document.getElementById("contact-form");
-  const formData = new FormData(form);
+    const formData = new FormData(event.target);
 
-  // Отправка данных на сервер
-  fetch("server-script.php", {
-    method: "POST",
-    body: formData,
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      if (data.success) {
-        alert("Данные успешно отправлены!");
-        closePopup();
-      } else {
-        alert("Произошла ошибка при отправке данных.");
-      }
+    fetch("php/send.php", {
+      method: "POST",
+      body: formData,
     })
-    .catch((error) => {
-      console.error("Ошибка:", error);
-    });
-}
-
-// Добавляем обработчик события для отправки формы
-document.getElementById("contact-form").addEventListener("submit", submitForm);
+      .then((response) => response.text())
+      .then((data) => {
+        if (data === "Сообщение успешно отправлено") {
+          console.log(data);
+        } else {
+          console.log("Произошла ошибка при отправке сообщения");
+        }
+      })
+      .catch((error) => {
+        console.error("Ошибка при отправке данных:", error);
+      });
+  });
 
 /*-----------------------slider--------------------*/
 
@@ -70,11 +61,9 @@ const swiper = new Swiper(".swiper", {
   effect: "creative",
   creativeEffect: {
     prev: {
-      // will set `translateZ(-400px)` on previous slides
       translate: [0, 0, -400],
     },
     next: {
-      // will set `translateX(100%)` on next slides
       translate: ["100%", 0, 0],
     },
   },
@@ -87,10 +76,8 @@ const swiper = new Swiper(".swiper", {
       // Обновите элемент с номером текущего слайда
       document.getElementById("currentSlide").textContent = currentSlide;
 
-      // Получите общее количество слайдов
       const totalSlides = this.slides.length;
 
-      // Обновите элемент с максимальным количеством слайдов
       document.getElementById("totalSlides").textContent = totalSlides;
     },
   },
@@ -102,8 +89,6 @@ const totalSlides = swiper.slides.length;
 document.getElementById("totalSlides").textContent = totalSlides;
 
 const swiper1 = document.querySelector(".swiper").swiper;
-
-// Now you can use all slider methods like
 swiper1.slideNext();
 
 const prevButton = document.querySelector(".carousel__arrow--left");
@@ -119,12 +104,10 @@ nextButton.addEventListener("click", () => {
 
 /*-----------------burger-menu-------------------*/
 
-// Получаем кнопку и список
 const burger = document.querySelector(".burger-menu");
 const list = document.querySelector(".nav");
 
-// Добавляем обработчик клика на кнопку
 burger.addEventListener("click", () => {
   burger.classList.toggle("active");
-  list.classList.toggle("active"); // Переключаем класс для применения анимации
+  list.classList.toggle("active");
 });
